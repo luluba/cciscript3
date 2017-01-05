953a89240504ce47fd6eb1f06860209a06b5ed45
mandyliu0305 <mandyliu0305@gmail.com>
2017-01-04 22:41:40 -0500

    test local branch

--- 1.6.py
+++ 1.6.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
 '''
 Given an image represented by an N*N matrix, where each pixel in the image is 4 bytes,write a method to rotate the image by 90 degree. Can you do this in place ? (nima, no)'''
+def rotate(matrix):
 
+
+def main():
+
+if __name__ == "__main__":
+	
