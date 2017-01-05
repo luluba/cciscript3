fe2f0ea2cff5b50b9894334c0159781dced63ece
hailStony2017 <yanwen.c.2010@gmail.com>
2017-01-04 23:52:29 -0500

    YW's branch

--- 1.5.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-'''
-Implement a method to perform basic string compression using the counts of repeated characters. For example: the string aabcccccaaa would become a2b1c5a3. if the "compressed" string would not become smaller than the original string. your method should return the original string. You can assume the string has only upper and lower case letter(a-z)
-'''
--- 1.6.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-'''
-Given an image represented by an N*N matrix, where each pixel in the image is 4 bytes,write a method to rotate the image by 90 degree. Can you do this in place ? (nima, no)'''
-
