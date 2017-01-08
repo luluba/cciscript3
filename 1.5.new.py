920d01aec61e47a0b52285c2016c91524336a5dc
hailStony2017 <yanwen.c.2010@gmail.com>
2017-01-08 12:47:55 -0500

    New problems in 6th edition Chapter 1

---
+++ 1.5.new.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+'''
+One Away:
+There are three types of edits that can be performed on strings:
+insert a character, remove a character, or replace a character.
+Given tow strings, write a function to check if they are one edit
+(or zero edits) away.
+
+EXAMPLE
+pale, ple -> true
+pales, pale -> true
+pale, bale -> true
+pale, bake -> false
+
+'''
+
