2999f86b1672f3287533a570dbcf6859c5d69624
mandyliu0305 <mandyliu0305@gmail.com>
2017-01-08 12:26:47 -0500

    solutions yay

--- 1.1.py
+++ 1.1.py
@@ -1,3 +1,38 @@
 #!/usr/bin/python
-#implement an algorithm to determine if a string has all unique characters. What if you cannot use additonal data strucutere
+#Implement an algorithm to determine if a string has all unique characters. What if you cannot use additonal data strucutere
 
+def is_uniq_str(str):
+	flag = {}
+	for s in str:
+		if s in flag:
+			return False
+		else:
+			flag[s] = 1
+	
+	return True 
+
+def is_uniq_str2(str):
+	for i in range(len(str)-1):
+		for j in range(i+1, len(str)):
+			if str[i] == str[j]:
+				return False
+	
+	return True
+			
+				
+
+def main():
+	test_str = "aabbcc"
+	print is_uniq_str(test_str)
+	print is_uniq_str2(test_str), '\n'
+
+	test_str2 = "abcdef"
+	print is_uniq_str(test_str2)
+	print is_uniq_str2(test_str2), '\n'
+
+	test_str3 = ""
+	print is_uniq_str(test_str3)
+	print is_uniq_str2(test_str3)
+
+if __name__== "__main__":
+	main()
--- 1.4.py
+++ 1.4.py
@@ -1,6 +1,25 @@
- #!/usr/bin/python
+#!/usr/bin/python
 #Write a method to replace all spaces in a string with '%20'.
 #You may assume that the string has sufficient space at the end of the string
 #to hold the additional characters, and that you are gvin the "true" length of the string
 #Input: "Mr John Smith   ", 13
 #Output: "Mr%20John%20Smith"
+
+def replace_with(str, length):
+	rel_str = "%20"
+	i = 0;
+	new_str = [] 
+	while i < length:
+		if str[i] == " ":
+			new_str.append(rel_str)
+		else:
+			new_str.append(str[i])
+		i += 1
+	return "".join(new_str)
+
+def main():
+	input = "Mr John Smith   "
+	print replace_with(input, 13)
+
+if __name__ == "__main__":
+	main()
