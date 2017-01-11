968ad6b84a8514a9030dcc2b42f3d56c09655825
hailStony2017 <yanwen.c.2010@gmail.com>
2017-01-11 00:37:48 -0500

    Improve func name Add 1.4.new.py

--- 1.3.py
+++ 1.3.py
@@ -6,7 +6,7 @@ Given two strings, write a method to decide if one is a permutation of the other
 
 import sys
 
-def is_strings_permutation(string_1, string_2):
+def are_strings_permutations(string_1, string_2):
     # Check if strings have same length
     if len(string_1) != len(string_2):
         print("Nah... These strings are not permutations of each other.")
@@ -34,4 +34,4 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python 1.3.py STRING1 STRING2")
     else:
-        is_strings_permutation(sys.argv[1], sys.argv[2])
+        are_strings_permutations(sys.argv[1], sys.argv[2])
--- 1.4.new.py
+++ 1.4.new.py
@@ -12,3 +12,32 @@ Output: True (permutations: "taco cat", "atco cta", etc.)
 
 '''
 
+import sys
+
+def is_string_palindrome_permutation(input_string):
+
+    # Create two dictionaries of ascii codes as hash table
+    ascii_char_dict = dict((i, 0) for i in range(128))
+
+    for i in range(len(input_string)):
+        c = input_string[i]
+        ascii_char_dict[ord(c)] += 1
+
+    # Count characters that have odd occurrence
+    odd_count = 0
+    for c in ascii_char_dict.keys():
+        if ascii_char_dict[c] % 2 == 1:
+            odd_count += 1
+        if odd_count > 1:
+            print("Nah, {0} is not a palindrome permutation".format(input_string))
+            return False
+
+    print("Yay, {0} is a palindrome permutation!".format(input_string))
+    return True
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python 1.4.new.py INPUT_STRING STRING")
+    else:
+        is_string_palindrome_permutation(sys.argv[1])
