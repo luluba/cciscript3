4d33d755f52ec47f1688471a75e969c56aa66b1f
hailStony2017 <yanwen.c.2010@gmail.com>
2017-01-11 00:45:20 -0500

    Using array

--- 1.1.py
+++ 1.1.py
@@ -10,11 +10,11 @@ structures?
 
 def string_has_unique_chars(input_string):
     # Create an dictionary of ascii codes as hash table
-    ascii_char_dict = dict((i, 0) for i in range(128))
+    ascii_char_array = [0] * 128
 
     for c in input_string:
-        ascii_char_dict[ord(c)] += 1
-        if ascii_char_dict[ord(c)] > 1:
+        ascii_char_array[ord(c)] += 1
+        if ascii_char_array[ord(c)] > 1:
             print "Nah...Duplicate characters found in string. "
             return False
 
