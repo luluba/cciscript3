a77a5d3442a86b0ac524dbbada5d3a54d6bb33ab
hailStony2017 <yanwen.c.2010@gmail.com>
2017-01-14 15:45:30 -0500

    Problem only

--- 1.3.py
+++ 1.3.py
@@ -1,21 +1,5 @@
 #!/usr/bin/python
-#Given two strings, write a method to decide if one is a permutation of the other.
-
-def is_perm(str1, str2):
-	if len(str1) != len(str2): return False
-
-	return sorted(str1) == sorted(str2)
-
-
-def main():
-	str1 = "abbcde"
-	str2 = "bbdeca"
-
-	str3 = "aabb"
-	str4 = "abcd"
-
-	print str1 , str2, is_perm(str1, str2)
-	print str3,  str4, is_perm(str3, str4)
-
-if __name__ == "__main__":
-	main()
+'''
+Check Permutation:
+Given two strings, write a method to decide if one is a permutation of the other.
+'''
